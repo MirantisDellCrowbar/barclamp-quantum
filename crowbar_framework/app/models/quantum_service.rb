568d466fa3c571153d6bb3977cdab0ff43c90d43
Maxim Datskovskiy <mdatskovsky@mirantis.com>
2013-08-29 16:14:12 +0300

    fix package path

--- crowbar_framework/app/models/quantum_service.rb
+++ crowbar_framework/app/models/quantum_service.rb
@@ -148,10 +148,8 @@ class QuantumService < ServiceObject
       admin_ip = nodes[0].get_network_by_type("admin")["address"]
       web_port = nodes[0]["provisioner"]["web_port"]
       # substitute the admin web portal
-      mellanox_tarball_path = role.default_attributes["quantum"]["mellanox_tarball"].gsub("<ADMINWEB>", "#{admin_ip}:#{web_port}")
-      ethtool_package_path = role.default_attributes["quantum"]["ethtool_package"].gsub("<ADMINWEB>", "#{admin_ip}:#{web_port}")
-      role.default_attributes["tempest"]["mellanox_tarball"] = tempest_tarball_path
-      role.default_attributes["tempest"]["ethtool_package"] = tempest_test_image_path
+      role.default_attributes["quantum"]["mellanox_tarball"].gsub!("<ADMINWEB>", "#{admin_ip}:#{web_port}")
+      role.default_attributes["quantum"]["ethtool_package"].gsub!("<ADMINWEB>", "#{admin_ip}:#{web_port}")
     end
 
     role.save
